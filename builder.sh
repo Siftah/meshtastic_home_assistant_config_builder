6190cc774327cc475c10a739b76d177637b9b275
John Baxendale <John@siftah.com>
2024-01-28 17:51:16 +0100

    Added Altitude/Latitude/Longitude

--- builder.sh
+++ builder.sh
@@ -2,7 +2,8 @@
 
 # This is used to build the topic from which we pull data
 # eg: /msh/2/json/LongFast/!f71e2dac
-mqtt_topic='meshtastic/2/json/LongFast/!f71e2dac'
+MQTTTOPIC='meshtastic\/2\/json\/LongFast\/\!f71e2dac'
+ALTITUDEMEASUREMENT='M'
 
 longnames=('Basestation' 'TBeam_02' 'RR_2c4d' 'Heltecv3_75f0' 'Meshtastic_1f88' 'Heltecv3_6610' 'Techo_77cf')
 shortnames=('base' 'tb02' 'rr' 'ht01' '1f88' 'ht02' 'jb')
@@ -12,5 +13,14 @@ for i in "${!longnames[@]}"
 do
 	echo "Building for: ${longnames[$i]} is ${shortnames[$i]} or ${stationids[$i]}"
 	cp template.txt ${shortnames[$i]}.yaml
-
+	sed -i '' "s/LONGNAME/${longnames[$i]}/g" ${shortnames[$i]}.yaml
+	sed -i '' "s/SHORTNAME/${shortnames[$i]}/g" ${shortnames[$i]}.yaml
+	sed -i '' "s/STATIONID/${stationids[$i]}/g" ${shortnames[$i]}.yaml
 done
+
+rm -f consolidated.yaml
+cat *.yaml >> consolidated.tmp
+rm -f *.yaml
+sed -i '' "s/MQTT_TOPIC/$MQTTTOPIC/g" consolidated.tmp
+sed -i '' "s/ALTITUDEMEASUREMENT/$ALTITUDEMEASUREMENT/g" consolidated.tmp
+mv consolidated.tmp consolidated.yaml
