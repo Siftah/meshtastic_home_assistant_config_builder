6b51bd7ffceead2d34566d760ab6457ccb61c836
John Baxendale <John@siftah.com>
2024-01-29 19:33:33 +0100

    Added the new Techo in.

--- builder.sh
+++ builder.sh
@@ -6,9 +6,9 @@ MQTTTOPIC='meshtastic\/2\/json\/LongFast\/\!f71e2dac'
 ALTITUDEMEASUREMENT='M'
 SENSOR_PREFIX='msh_'
 
-longnames=('Basestation' 'TBeam_02' 'RR_2c4d' 'Heltecv3_75f0' 'Meshtastic_1f88' 'Heltecv3_6610' 'Techo_77cf')
-shortnames=('base' 'tb02' 'rr' 'ht01' '1f88' 'ht02' 'jb')
-stationids=('4145950124' '4064721948' '1721248845' '3806623216' '2988711816' '3806488592' '1578268623')
+longnames=('Basestation' 'TBeam_02' 'RR_2c4d' 'Heltecv3_75f0' 'Meshtastic_1f88' 'Heltecv3_6610' 'Techo_77cf' 'Techo_f2bf')
+shortnames=('base' 'tb02' 'rr' 'ht01' '1f88' 'ht02' 'jb' 'te02')
+stationids=('4145950124' '4064721948' '1721248845' '3806623216' '2988711816' '3806488592' '1578268623' '1344991935')
 
 for i in "${!longnames[@]}"
 do
