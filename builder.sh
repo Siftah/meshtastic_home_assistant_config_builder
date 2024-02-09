bab07af9fbb0ca26debb6e58b0de3a43dcae10a1
John Baxendale <siftah@Johns-Air.siftah.net>
2024-02-09 23:45:31 +0100

    Add the updated sed reference

--- builder.sh
+++ builder.sh
@@ -17,19 +17,19 @@ do
 	echo "Building for: ${longnames[$i]} is ${shortnames[$i]} or ${stationids[$i]}"
 	cp mqtt.txt ${shortnames[$i]}_mqtt.yaml
 	cp automations.txt ${shortnames[$i]}_automations.yaml
-	sed -i '' "s/LONGNAME/${longnames[$i]}/g" ${shortnames[$i]}_mqtt.yaml
-	sed -i '' "s/SHORTNAME/${shortnames[$i]}/g" ${shortnames[$i]}_mqtt.yaml
-	sed -i '' "s/STATIONID/${stationids[$i]}/g" ${shortnames[$i]}_mqtt.yaml
-	sed -i '' "s/SHORTNAME/${shortnames[$i]}/g" ${shortnames[$i]}_automations.yaml
-	sed -i '' "s/UNIQUEID/$UNIQUEID/g" ${shortnames[$i]}_automations.yaml	
+	$SEDCOMMAND "s/LONGNAME/${longnames[$i]}/g" ${shortnames[$i]}_mqtt.yaml
+	$SEDCOMMAND "s/SHORTNAME/${shortnames[$i]}/g" ${shortnames[$i]}_mqtt.yaml
+	$SEDCOMMAND "s/STATIONID/${stationids[$i]}/g" ${shortnames[$i]}_mqtt.yaml
+	$SEDCOMMAND "s/SHORTNAME/${shortnames[$i]}/g" ${shortnames[$i]}_automations.yaml
+	$SEDCOMMAND "s/UNIQUEID/$UNIQUEID/g" ${shortnames[$i]}_automations.yaml	
 done
 
 rm -f consolidated_mqtt.yaml consolidate_automations.yaml
 cat *_automations.yaml >> consolidated_automations.tmp
 cat *_mqtt.yaml >> consolidated_mqtt.tmp
 rm -f *.yaml
-sed -i '' "s/MQTT_TOPIC/$MQTTTOPIC/g" consolidated_mqtt.tmp
-sed -i '' "s/ALTITUDEMEASUREMENT/$ALTITUDEMEASUREMENT/g" consolidated_mqtt.tmp
-sed -i '' "s/SENSOR_PREFIX/$SENSOR_PREFIX/g" consolidated_mqtt.tmp consolidated_automations.tmp
+$SEDCOMMAND "s/MQTT_TOPIC/$MQTTTOPIC/g" consolidated_mqtt.tmp
+$SEDCOMMAND "s/ALTITUDEMEASUREMENT/$ALTITUDEMEASUREMENT/g" consolidated_mqtt.tmp
+$SEDCOMMAND "s/SENSOR_PREFIX/$SENSOR_PREFIX/g" consolidated_mqtt.tmp consolidated_automations.tmp
 mv consolidated_mqtt.tmp consolidated_mqtt.yaml
 mv consolidated_automations.tmp consolidated_automations.yaml
