be23f846f9ecc1e54e7df35ddff32ff9813ec1fa
John Baxendale <John@siftah.com>
2024-02-09 18:28:03 +0100

    Now creates the automations.yaml to create device tracker entries with lat/lon for each node.

--- builder.sh
+++ builder.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+TIMESTAMP=`date +%Y%m%d%H%M%S`
 
 # This is used to build the topic from which we pull data
 # eg: /msh/2/json/LongFast/!f71e2dac
@@ -6,23 +7,31 @@ MQTTTOPIC='meshtastic\/2\/json\/LongFast\/\!f71e2dac'
 ALTITUDEMEASUREMENT='M'
 SENSOR_PREFIX='msh_'
 
-longnames=('Basestation' 'TBeam_02' 'RR_2c4d' 'Heltecv3_75f0' 'Meshtastic_1f88' 'Heltecv3_6610' 'Techo_77cf' 'Techo_f2bf')
-shortnames=('base' 'tb02' 'rr' 'ht01' '1f88' 'ht02' 'jb' 'te02')
-stationids=('4145950124' '4064721948' '1721248845' '3806623216' '2988711816' '3806488592' '1578268623' '1344991935')
+
+longnames=('Basestation' 'TBeam_02' 'RR_2c4d' 'Heltecv3_75f0' 'TBeam_01' 'Heltecv3_6610' 'Techo_77cf' 'Techo_f2bf' 'NanoG2Ultra_7673')
+shortnames=('base' 'tb02' 'rr' 'ht01' 'tb01' 'ht02' 'te01' 'te02' 'g201')
+stationids=('4145950124' '4064721948' '1721248845' '3806623216' '2988711816' '3806488592' '1578268623' '1344991935' '3186390643')
 
 for i in "${!longnames[@]}"
 do
+	UNIQUEID=${TIMESTAMP}${i}
+
 	echo "Building for: ${longnames[$i]} is ${shortnames[$i]} or ${stationids[$i]}"
-	cp template.txt ${shortnames[$i]}.yaml
-	sed -i '' "s/LONGNAME/${longnames[$i]}/g" ${shortnames[$i]}.yaml
-	sed -i '' "s/SHORTNAME/${shortnames[$i]}/g" ${shortnames[$i]}.yaml
-	sed -i '' "s/STATIONID/${stationids[$i]}/g" ${shortnames[$i]}.yaml
+	cp mqtt.txt ${shortnames[$i]}_mqtt.yaml
+	cp automations.txt ${shortnames[$i]}_automations.yaml
+	sed -i '' "s/LONGNAME/${longnames[$i]}/g" ${shortnames[$i]}_mqtt.yaml
+	sed -i '' "s/SHORTNAME/${shortnames[$i]}/g" ${shortnames[$i]}_mqtt.yaml
+	sed -i '' "s/STATIONID/${stationids[$i]}/g" ${shortnames[$i]}_mqtt.yaml
+	sed -i '' "s/SHORTNAME/${shortnames[$i]}/g" ${shortnames[$i]}_automations.yaml
+	sed -i '' "s/UNIQUEID/$UNIQUEID/g" ${shortnames[$i]}_automations.yaml	
 done
 
-rm -f consolidated.yaml
-cat *.yaml >> consolidated.tmp
+rm -f consolidated_mqtt.yaml consolidate_automations.yaml
+cat *_automations.yaml >> consolidated_automations.tmp
+cat *_mqtt.yaml >> consolidated_mqtt.tmp
 rm -f *.yaml
-sed -i '' "s/MQTT_TOPIC/$MQTTTOPIC/g" consolidated.tmp
-sed -i '' "s/ALTITUDEMEASUREMENT/$ALTITUDEMEASUREMENT/g" consolidated.tmp
-sed -i '' "s/SENSOR_PREFIX/$SENSOR_PREFIX/g" consolidated.tmp
-mv consolidated.tmp consolidated.yaml
+sed -i '' "s/MQTT_TOPIC/$MQTTTOPIC/g" consolidated_mqtt.tmp
+sed -i '' "s/ALTITUDEMEASUREMENT/$ALTITUDEMEASUREMENT/g" consolidated_mqtt.tmp
+sed -i '' "s/SENSOR_PREFIX/$SENSOR_PREFIX/g" consolidated_mqtt.tmp consolidated_automations.tmp
+mv consolidated_mqtt.tmp consolidated_mqtt.yaml
+mv consolidated_automations.tmp consolidated_automations.yaml
